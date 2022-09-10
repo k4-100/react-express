100577bcc3613bd7c33cab76dcd5d8aad3602144
k4-100 <k4-100@protonmail.com>
2022-09-10 15:07:31 +0200

    Finished mock table

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,16 +1,49 @@
-import { Table, TableCell, TableHead, TableBody } from "@mui/material";
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableBody,
+  TableRow,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
 import React from "react";
 
 const CustomTable: React.FC = () => {
   return (
-    <Table>
+    <Table
+      sx={{
+        "& td": {
+          border: `${grey[400]} 1px solid `,
+        },
+      }}
+    >
       <TableHead>
-        <TableCell>foodID</TableCell>
-        <TableCell>productName</TableCell>
-        <TableCell>priceForUnit</TableCell>
-        <TableCell>unitsStored</TableCell>
-        <TableCell>unit</TableCell>
+        <TableRow
+          sx={{
+            // "& td": {
+            //   border: `${grey[700]} 1px solid`,
+            //   fontSize: 10,
+            //   textAlign: "center",
+            // },
+            backgroundColor: grey[700],
+          }}
+        >
+          <TableCell>foodID</TableCell>
+          <TableCell>productName</TableCell>
+          <TableCell>priceForUnit</TableCell>
+          <TableCell>unitsStored</TableCell>
+          <TableCell>unit</TableCell>
+          <TableCell>delete?</TableCell>
+        </TableRow>
       </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>foodID</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>foodID</TableCell>
+        </TableRow>
+      </TableBody>
     </Table>
   );
 };
