e9880b1a95a3b251aa33317d49d720c276d39cc0
k4-100 <k4-100@protonmail.com>
2022-09-19 15:14:47 +0200

    Created basic connection for mysql

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
 const PORT = 5000;
 const HOSTNAME = "127.0.0.1";
+
+const dbConnection = mysql.createConnection({
+  host: HOSTNAME,
+  user: "root",
+  password: "1234",
+  database: "Products",
+});
+
+dbConnection.connect();
+
+dbConnection.query("SELECT * FROM Products.Food AS res", (error, results) => {
+  if (error) throw error;
+
+  console.log(results);
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1> connected </h1>");
